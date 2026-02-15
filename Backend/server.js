136885499e6dd463aae76492359e36d500d8d624
Shelarnitin <shelarnitin399@gmail.com>
2026-02-15 15:36:19 +0530

    fixed PORT

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.Port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
